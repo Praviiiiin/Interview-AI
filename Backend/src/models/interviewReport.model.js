51a7e7261f9e696aafc7d1cd2963f7f818525133
Pravin <epravin2005@gmail.com>
2026-06-04 15:35:47 +0530

    feat: add user reference to interview report schema and create interview routes for report generation

--- Backend/src/models/interviewReport.model.js
+++ Backend/src/models/interviewReport.model.js
@@ -84,7 +84,11 @@ const interviewReportScehma = new mongoose.Schema({
     technicalQuestions : [ technicalQuestionSchema ],
     behavioralQuestions : [ behavioralQuestionSchema ],
     skillGaps : [ skillGapSchema ],
-    preparationPlan: [ preparationPlanSchema ]
+    preparationPlan: [ preparationPlanSchema ],
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users"
+    }
 
 }, {
     timestamps: true
---
+++ Backend/src/routes/interview.routes.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const { authUser } = require("../middlewares/auth.middleware")
+const interviewController = require("../controllers/interview.controller")
+const upload = require("../middlewares/file.middleware")
+
+const interviewRouter = express.Router()
+
+
+/**
+ * @route POST /api/interview/
+ * @description generate new interview report on the basis of user self description, resume pdf and job description  
+ * @access private
+ */
+
+interviewRouter.post("/", authUser, upload.single("resume"), interviewController.generateInterviewReportController)
+
+module.exports = interviewRouter
