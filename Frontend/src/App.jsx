032ba4052a6fdf1cd225ddd7126095ff768adb52
Pravin <epravin2005@gmail.com>
2026-06-10 23:47:51 +0530

    feat: add AnimatedBackground component to App for enhanced visual experience

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { RouterProvider } from "react-router"
 import { router } from "./app.routes.jsx"
 import { AuthProvider } from "./features/auth/auth.context.jsx"
 import { InterviewProvider } from "./features/interview/interview.context.jsx"
+import AnimatedBackground from "./components/AnimatedBackground.jsx"
 
 function App() {
   
   return (
     <AuthProvider>
       <InterviewProvider>
+        <AnimatedBackground />
         <RouterProvider router ={router} />
       </InterviewProvider>      
     </AuthProvider>
